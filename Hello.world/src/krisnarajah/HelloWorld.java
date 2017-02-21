412b095d71e712dbb869f760ad3f575b127e1ef5
323906552 <323906552@C-4BDQKS1.tdsb.on.ca>
2017-02-21 11:22:31 -0500

    My first push

---
+++ Hello.world/src/krisnarajah/HelloWorld.java
@@ -0,0 +1,18 @@
+package krisnarajah;
+
+/** 
+ *  Hello.world.java
+ *  Print out, "Hello world"
+ * February,21 2017
+ * @author Gayathiry Krisnarjah
+ 
+ */
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Hello World!");
+	}
+}
